927330b540ec6a5d6f43bc91255333607868dfdc
Nicola <89164508+NicolaDerocchi22@users.noreply.github.com>
2025-05-08 17:02:21 +0200

    aggiunti primi componenti

--- src/components/InputSection.tsx
+++ src/components/InputSection.tsx
@@ -1,5 +1,44 @@
 const InputSection = () => {
-  return <div className='border p-4 rounded-xl'>InputSection</div>;
+  return (
+    <div className='border p-4 rounded-xl h-min align-middle'>
+      <h2 className='text-xl font-bold mb-4'>Input</h2>
+      <form className='flex flex-col gap-4'>
+        <div className='flex flex-row gap-2'>
+          <input
+            type='text'
+            placeholder='Parametro 1'
+            className='border p-2 rounded-md flex-1'
+          />
+          <input
+            type='text'
+            placeholder='Parametro 2'
+            className='border p-2 rounded-md flex-1'
+          />
+        </div>
+        <div className='flex flex-row gap-2'>
+          <input
+            type='text'
+            placeholder='Parametro 3'
+            className='border p-2 rounded-md flex-1'
+          />
+          <input
+            type='text'
+            placeholder='Parametro 4'
+            className='border p-2 rounded-md flex-1'
+          />
+        </div>
+        <div className='border-b border-gray-500 w-full h-0 my-2' />
+        <textarea
+          placeholder='Cosa vorresti sapere?'
+          className='border p-2 rounded-md'
+          rows={5}
+        />
+        <button className='bg-blue-500 text-white p-2 rounded-md'>
+          Chiedi
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default InputSection;
--- src/components/ResponseSection.tsx
+++ src/components/ResponseSection.tsx
@@ -1,6 +1,44 @@
 const ResponseSection = () => {
   return (
-    <div className='border col-span-2 p-4 rounded-xl'>ResponseSection</div>
+    <div className='border p-4 rounded-xl col-span-2 h-min'>
+      <h2 className='text-xl font-bold mb-4'>Response</h2>
+      <p className='border border-gray-300 p-4 rounded-md mb-4'>
+        {' '}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
+        facilisis nisl eget aliquet sodales. Morbi mollis velit porta, congue
+        leo vel, tincidunt odio. Aenean laoreet velit at velit placerat, vel
+        tincidunt nisi interdum. Morbi quis velit urna. Morbi mi tortor,
+        pulvinar ac tristique a, pharetra eget dui. Suspendisse potenti. Nam sit
+        amet accumsan massa. Mauris purus purus, tristique consectetur tellus
+        ac, bibendum consequat ex. Sed euismod vulputate risus id porttitor.
+        Mauris iaculis accumsan fermentum. Mauris nulla enim, auctor mollis diam
+        ac, porttitor venenatis lectus. In eu nisi purus. In ex massa, vulputate
+        non lorem vitae, rhoncus dictum risus. In dignissim ultrices venenatis.
+        Nulla pharetra lobortis eleifend. Aenean dapibus mi tincidunt tincidunt
+        porta. Vivamus lectus arcu, auctor ac sapien quis, imperdiet eleifend
+        mi. Aliquam condimentum elit id felis venenatis, in tempor lectus
+        vestibulum. Quisque eros ex, commodo sed velit non, pharetra pretium
+        urna. Nam fringilla sagittis felis nec luctus. Nunc quis ultricies nisl,
+        id gravida nulla. Aliquam sit amet vestibulum tortor, et tincidunt
+        nulla. Vivamus mattis lectus ac dignissim dictum. Morbi posuere suscipit
+        nulla sit amet fringilla. Etiam sodales bibendum turpis id tincidunt. Ut
+        vitae erat ex. Aliquam vel ultricies quam, ut tristique leo. Aenean
+        magna lorem, hendrerit eget venenatis id, mattis non erat. Nullam non
+        fringilla arcu. Pellentesque lacinia tincidunt tristique. Donec
+        elementum ante ut placerat aliquam. Integer et metus nec justo dapibus
+        tincidunt eleifend vitae augue. Aliquam dictum diam quis dui consequat
+        ultrices. Vivamus orci nisl, sollicitudin sed metus venenatis, lobortis
+        tempus odio. Nam efficitur ornare leo vitae malesuada. Cras hendrerit
+        sit amet turpis sit amet ultricies. Maecenas efficitur maximus lacus,
+        sit amet ultrices ligula dignissim a. Vestibulum sed tincidunt dui.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod
+        eros vitae sapien egestas, a laoreet nulla blandit. Vestibulum fermentum
+        ut nisi eu sodales. Quisque at vestibulum lorem. Donec sollicitudin mi
+        risus, id bibendum ante vulputate vitae. Quisque elementum pulvinar
+        vehicula. Integer feugiat erat egestas, convallis orci vitae, cursus
+        eros.{' '}
+      </p>
+    </div>
   );
 };
 
