dd3977da9e18de10a91fc69a3474176146388723
Elia Pede <38972600+0EliaPede0@users.noreply.github.com>
2025-06-16 09:55:53 +0200

    Final response is now different

--- src/components/StreamResponse.tsx
+++ src/components/StreamResponse.tsx
@@ -1,13 +1,23 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import ResponseBox from './ResponseBox';
+import ResponseBoxElementNotText from './ResponseBoxElementNotText';
 
 const StreamResponse: React.FC<{
   showPartialResponses: boolean;
   streamingResponsesByChunk: any;
   isLoading: boolean;
 }> = ({ showPartialResponses, streamingResponsesByChunk, isLoading }) => {
-  useEffect(() => {}, [streamingResponsesByChunk]);
+  const [finalResponse, setFinalResponse] = useState<(string | undefined)[]>([]);
+  useEffect(() => {
+    setFinalResponse(
+      Object.entries(streamingResponsesByChunk).map(([chunkId, content]) => {
+        if (typeof content === 'string' && chunkId === '-3') {
+          return content;
+        }
+      })
+    );
+  }, [streamingResponsesByChunk]);
   const ids = ['1', '2', '3'];
   return (
     <>
@@ -45,6 +55,22 @@ const StreamResponse: React.FC<{
             );
           }
         })}
+      </div>
+
+      <div>
+        <ResponseBoxElementNotText
+          element={
+            (() => {
+              return <>
+                <p className='text-lg font-semibold'>Finale Pede</p>
+                <div className='divider mt-0' />
+                <div>
+                  <p className='whitespace-pre-line'>{finalResponse}</p>
+                </div>
+              </>
+            })()
+          }
+          isLoading={isLoading} />
       </div>
     </>
   );
